612629b18390f74e8ee0a8eeac6cb6ce5656a621
dsankdnasldas <3216497571@qq.com>
2026-06-16 18:46:35 +0800

    1

---
+++ src/seetfacesever.cpp
@@ -0,0 +1,35 @@
+#include "seetfacesever.h"
+#include "ui_seetfacesever.h"
+seetfacesever::seetfacesever(QWidget* parent)
+    : QMainWindow(parent)
+    , ui(new Ui_seetfacesever)
+{
+    ui->setupUi(this);
+
+    connect(&mserver, &QTcpServer::newConnection, this, &seetfacesever::accept_client);
+    mserver.listen(QHostAddress::Any,9999);
+}
+
+seetfacesever::~seetfacesever()
+{
+    delete ui; 
+}
+
+//接受客户端连接
+void seetfacesever::accept_client()
+{
+    //获取与客户端通信的套接字
+    msocket = mserver.nextPendingConnection();
+
+    //当客户端有数据到达会发送readyRead信号
+    connect(msocket,&QTcpSocket::readyRead,this,&seetfacesever::read_data);
+}
+
+//读取客户端发送的数据
+void seetfacesever::read_data()
+{
+    //读取所有的数据
+    QString msg = msocket->readAll();
+    qDebug()<<msg;
+
+}
---
+++ src/seetfacesever.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "ui_seetfacesever.h"
+#include <QMainWindow>
+#include <QTcpSocket>
+#include <QTcpServer>
+
+class seetfacesever : public QMainWindow {
+    Q_OBJECT
+    
+public:
+    seetfacesever(QWidget* parent = nullptr);
+    ~seetfacesever();
+
+protected slots:
+    void accept_client();
+    void read_data();
+
+private:
+    Ui_seetfacesever* ui;
+    QTcpServer mserver;
+    QTcpSocket *msocket;
+};
